0c826610fcae38058839e93a834d207030999bbe
Stefan Djindjic <stefan.djindjic@bstorm.co>
2017-04-24 13:13:46 +0200

    lessone 27

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -43,6 +43,7 @@ class PostController extends Controller
         // validate the data
         $this->validate($request, array(
             'title' => 'required|max:255',
+            'slug' => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
             'body' => 'required'
         ));
 
@@ -50,6 +51,7 @@ class PostController extends Controller
         $post = new Post;
 
         $post->title = $request->title;
+        $post->slug = $request->slug;
         $post->body = $request->body;
 
         $post->save();
@@ -99,6 +101,7 @@ class PostController extends Controller
         // validate the data
         $this->validate($request, array(
             'title' => 'required|max:255',
+            'slug' => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
             'body' => 'required'
         ));
 
@@ -106,6 +109,7 @@ class PostController extends Controller
         $post = Post::find($id);
 
         $post ->title = $request->input('title');
+        $post ->slug = $request->input('slug');
         $post ->body = $request->input('body');
 
         $post->save();
