05a63d5a83fe15bfd34ab041d160100760d4d9a9
Djindja <stefansdjindjic@gmail.com>
2017-04-25 22:51:14 +0200

    fixes

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -10,9 +10,9 @@ class BlogController extends Controller
 {
     public function getSingle($slug) {
         // fetch from the DB based on slug
-        $post = Post::where('slug', '=', $slug)->get();
-        
+        $post = Post::where('slug', '=', $slug)->first();
 
         // return the view and pass in the post object
+        return view('blog.single')->withPost($post);
     }
 }
