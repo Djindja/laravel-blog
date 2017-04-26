cddfabe81dca54eb21641901e98786349da1def8
Stefan Djindjic <stefan.djindjic@bstorm.co>
2017-04-26 11:16:22 +0200

    part30

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
+
+    public function getIndex() {
+        $posts = Post::paginate(2);
+
+        return view('blog.index')->withPosts($posts);
+    }
+
     public function getSingle($slug) {
         // fetch from the DB based on slug
         $post = Post::where('slug', '=', $slug)->first();
