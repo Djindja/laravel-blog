a4f42eaa34dbaaf4bcc32478b48399393329e95d
Stefan Djindjic <stefan.djindjic@bstorm.co>
2017-05-04 17:14:09 +0200

    fixes

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,30 @@
+@extends('main')
+
+@section('title', '| All Categories')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-8">
+            <h1>Categories</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach($categories as $category)
+                        <tr>
+                            <td>{{ $category->id }}</td>
+                            <td>{{ $category->name }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+@endsection
