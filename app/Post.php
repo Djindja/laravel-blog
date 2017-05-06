fd795d9b8dd2ae0eb5e24d066f1ee5b3b043af6c
Djindja <stefansdjindjic@gmail.com>
2017-05-06 21:52:52 +0200

    fix

--- app/Post.php
+++ app/Post.php
@@ -13,4 +13,8 @@ class Post extends Model
     public function tags() {
         return $this->belongsToMany('App\Tag');
     }
+
+    public function comments() {
+        return $this->nasMany('App\Comment');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,13 +30,15 @@ Route::group(['middleware' => ['web']], function(){
     Route::resource('categories', 'CategoryController', ['except' => ['create']]);
     Route::resource('tags', 'TagController', ['except' => ['create']]);
 
+    // Comments
+    Route::post('comments/{post_id}', ['uses' => 'CommentsController@store', 'as' => 'comments.store']);
 
     Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
     Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
     Route::get('/', 'PagesController@getIndex');
     Route::get('about', 'PagesController@getAbout');
     Route::get('contact', 'PagesController@getContact');
-    
+
     Route::post('/contact', 'PagesController@postContact');
 
     Route::resource('posts', 'PostController');
